81d23e25190bbb39d47423a42a1fcca492097341
Pavlo Karpa <pavlo.karpa17@gmail.com>
2020-03-15 22:01:13 +0200

    Revert "Revert "Add models for news pagination""
    This reverts commit 495aaba1d29d28f800bf0186021311080f266d6b.

---
+++ CompetitionApp/CompetitionApp/Models/IndexView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetitionApp.Models
+{
+    public class IndexView
+    {
+        public IEnumerable<NewsImage> NewsImage { get; set; }
+        public PageView PageView { get; set; }
+    }
+}
