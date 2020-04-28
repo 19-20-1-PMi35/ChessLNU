29cec3ee1e5f8ce3738515c2154a02f112ad90d1
nsorokopud <34284524+nsorokopud@users.noreply.github.com>
2020-04-28 13:27:32 +0300

    implemented news commenting

---
+++ CompetitionApp/CompetitionApp/Models/NewsComment.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace CompetitionApp.Models
+{
+    public class NewsComment
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+        public string UserId { get; set; }
+        public int NewsId { get; set; }
+        public int ParentId { get; set; }
+    }
+}
